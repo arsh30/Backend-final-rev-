c1d1f58465d4ab30ea9261d08f6f60281c129158
Arsh <as3096734@gmail.com>
2021-11-25 20:33:13 -0800

    class 3 finish upto update signupuser, jwt,mongodb

--- revision/app1.js
+++ revision/app1.js
@@ -1,22 +1,28 @@
 //npm init -y
 const express = require("express");
 let fs = require("fs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const userModel = require('./model/userModel');
+const { JWT_SECRET } = require("./secret");
+
 const app = express();
 
 let content = JSON.parse(fs.readFileSync("./data.json"));
 //crud functions
 
-app.use(express.json());
 app.use(express.static("Frontend_Folder"));
+app.use(express.json());
+app.use(cookieParser()); //from this we send cookie to anyone (cookie ie token)
 
 // Make routes in particular folder
 const userRouter = express.Router();
 const authRouter = express.Router();
 
-app.use("/user", userRouter);
-app.use("/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-userRouter.route("/user").post(bodyChecker, createUser); //bodyChecker is the middle ware -> toh uske aagla chljaega function && we send middleware next or jo next hoga toh uske next chljaega
+// userRouter.route("/user").post(bodyChecker, createUser); //bodyChecker is the middle ware -> toh uske aagla chljaega function && we send middleware next or jo next hoga toh uske next chljaega
 userRouter.route("/").get(protectRoute, getUsers);
 
 // userRouter
@@ -41,49 +47,58 @@ function bodyChecker(req, res, next) {
 }
 
 function protectRoute(req, res, next) {
-  //it is used for authentication if we do multiple time login
   console.log("protectRoute reached");
+  console.log("cookies", req.cookies); //cookieparser
 
-  let isAllowed = false; //1) here we pass the jwt
-  if (isAllowed) {
-    //2) verify everytime that if bringing the token to get your response
+  //token verify -> do this when we do the reqest multiple times
+  let decryptedToken = jwt.verify(req.cookies.JWT, JWT_SECRET); //req.cookies.JWT -> JWTis the cookie name jo request me thi AND 2nd argument is the secret code
+  console.log("decryptedToken", decryptedToken);
+
+  //2) verify everytime that if bringing the token to get your response
+  if (decryptedToken) {
     next();
   } else {
     res.send("kindly login to access this resource");
   }
 }
 
-function signupUser(req, res) {
-  let { name, email, password, confirmPassword } = req.body;
-  if (password == confirmPassword) {
-    let newUser = { name, email, password };
-    content.push(newUser); //1st put entry
-
-    fs.writeFileSync("./data.json", JSON.stringify(content)); //2nd save in dataStorage
+async function signupUser(req, res) {
+  try {
+    let newUser = await userModel.create(req.body);  //create is used to entries in the document
     res.status(200).json({
-      createdUser: newUser,
-    });
-  } else {
-    res.status(201).json({
-      message: "password or confirmPassword are not match",
-    });
+      message: "user created successfully",
+      user: newUser
+    })
+  } catch (err) {
+    res.status(500).json({
+      message: "error"
+    })
   }
 }
 
 function loginUser(req, res) {
-  let { email, password } = req.body;
-  let obj = content.find(function (obj) {
-    //1) Database pr loop lgaya or search kra id ko
-    return (obj.email = email); //rhs is email which is send through the frontend
-  });
-  if (obj.password == password) {
-    res.status(200).json({
-      message: "user logged In",
-      user: obj,
+  try {
+    let { email, password } = req.body;
+    let obj = content.find(function (obj) {
+      //1) Database pr loop lgaya or search kra id ko
+      return (obj.email = email); //rhs is email which is send through the frontend
     });
-  } else {
-    res.status(402).json({
-      message: "kindly go on the login function",
+    if (obj.password == password) {
+      let token = jwt.sign({ email: obj.email }, JWT_SECRET); //1st param is payload(email -> yeh hum khud kuch bhi lik skte hai :email jidr se nikali h vo), 2nd parm is the SECRET KEY Which we make
+      console.log("tokenId", token);
+      res.cookie("JWT", token, { httpOnly: true }); //1ST PARAM is Cookie Name(khud se kuch bhi rakho) , 2nd param is(token)
+      res.status(200).json({
+        message: "user logged In",
+        user: obj,
+      });
+    } else {
+      res.status(402).json({
+        message: "kindly go on the login function",
+      });
+    }
+  } catch (err) {
+    res.json({
+      message: "Kindly do signup first",
     });
   }
 }
@@ -107,11 +122,11 @@ function getUsers(req, res) {
   });
 }
 
-function getUser(req, res) {
-  res.send({
-    message: content,
-  });
-}
+// function getUser(req, res) {
+//   res.send({
+//     message: content,
+//   });
+// }
 
 app.listen(8082, function () {
   console.log("server is started");
---
+++ revision/model/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const { DB_PASSWORD } = require('../secret');
+const validator = require("email-validator");
+
+let db_link =
+    `mongodb+srv://admin:${DB_PASSWORD}@cluster0.eeqwe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+mongoose.connect(db_link).then(function (connection) {
+    console.log('Database is connected');
+}).catch(function (err) {
+    console.log('err',err);
+})
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: function () {
+            //3rd party library: npm package
+            return validator.validate(this.email);  //this.email means issi schema ki email
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    confirmPassword: {
+        type: String,
+        required: true,
+        min: 8,
+        validate: function () {
+            this.password = this.confirmPassword
+        }
+    },
+    createdAt: {
+        type:String
+    }
+})
+
+//2nd make model from userschema
+const userModel = mongoose.model("userModel", userSchema);
+
+//to use this we export this module
+module.exports = userModel;
